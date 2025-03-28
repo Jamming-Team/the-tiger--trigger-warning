0c9ed073a0c76aee574a8a893fd0b29801f3066d
xidriel <stormingisle@proton.me>
2025-03-28 16:06:00 +0300

    Added EventBus

--- Assets/Tiger/Code/Core/ScenesManagement/CoreBootstrapper.cs
+++ Assets/Tiger/Code/Core/ScenesManagement/CoreBootstrapper.cs
@@ -5,7 +5,7 @@ namespace Tiger {
     public class CoreBootstrapper {
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         static async void Init() {
-            Debug.Log("Bootstrapper init");
+            Debug.Log("Core Bootstrapper Init");
             await SceneManager.LoadSceneAsync(SceneNames.CORE, LoadSceneMode.Additive);
         }
     }
